be34c91d535ef40c1f4886f0803d47a438ae0736
vald3nir <vald3nir@gmail.com>
2017-12-13 09:40:04 -0300

    modification in project styles

--- app/src/main/java/vald3nir/programming_challenge/views/MultimediaActivity.java
+++ app/src/main/java/vald3nir/programming_challenge/views/MultimediaActivity.java
@@ -1,13 +1,18 @@
 package vald3nir.programming_challenge.views;
 
 import android.annotation.SuppressLint;
+import android.content.res.Configuration;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.VideoView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.UiThread;
@@ -32,10 +37,42 @@ public class MultimediaActivity extends AppCompatActivity {
     String baseUrl;
 
     @ViewById
+    public Toolbar toolbar;
+
+    @ViewById
     VideoView videoViewRelative;
 
     MediaPlayer mediaPlayerAudio = new MediaPlayer();
     MediaPlayer mediaPlayerVideo = new MediaPlayer();
+
+    int currentRuntime;
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        currentRuntime = mediaPlayerAudio.getCurrentPosition();
+        System.out.println();
+    }
+
+    @SuppressLint("RestrictedApi")
+    @AfterViews
+    public void afterViews() {
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
 
     @Override
     protected void onPause() {
@@ -73,6 +110,7 @@ public class MultimediaActivity extends AppCompatActivity {
 
                 mediaPlayerAudio.setDataSource(dataAssets.getAssetsLocation() + "/" + multimedia.getAudio());
                 mediaPlayerAudio.prepare();
+                mediaPlayerAudio.seekTo(currentRuntime);
 
             } catch (IOException e) {
                 e.printStackTrace();
