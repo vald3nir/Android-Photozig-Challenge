fe74a9905e9f5361e5085f4489fd1981615e40f4
vald3nir <vald3nir@gmail.com>
2017-12-13 10:19:56 -0300

    Adding treatment when the video file does not exist

---
+++ app/src/main/java/vald3nir/programming_challenge/views/VideoDownloadCallback.java
@@ -0,0 +1,13 @@
+package vald3nir.programming_challenge.views;
+
+/**
+ * Created by vald3nir on 13/12/17
+ */
+
+public interface VideoDownloadCallback {
+
+    void notifyDownloadComplete();
+
+    void notifyDownloadCanceled();
+
+}
